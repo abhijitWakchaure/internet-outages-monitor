03d4f49b0e7d3940c547e3d3e01bd977f6e614e1
Abhijit Wakchaure <abhijitwakchaure.2014@gmail.com>
2023-01-02 23:31:23 +0530

    build for arm64

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 VERSION=$(cat VERSION)
 APP_NAME="internet-outages-monitor"
 OUTFILE_LINUX="${APP_NAME}-v${VERSION}-linux_amd64"
-OUTFILE_ARM="${APP_NAME}-v${VERSION}-linux_arm32"
+OUTFILE_ARM_32="${APP_NAME}-v${VERSION}-linux_arm32"
+OUTFILE_ARM_64="${APP_NAME}-v${VERSION}-linux_arm64"
 OUTFILE_WINDOWS="${APP_NAME}-v${VERSION}-windows_amd64.exe"
 OUTFILE_DARWIN="${APP_NAME}-v${VERSION}-darwin_amd64"
 
@@ -16,8 +17,11 @@ export CGO_ENABLED=0
 echo "Building for linux..."
 GOOS=linux GOARCH=amd64 go build -o "dist/${OUTFILE_LINUX}" -ldflags '-w -s -extldflags "-static"' .
 
-echo "Building for arm..."
-GOOS=linux GOARCH=arm GOARM=7 go build -o "dist/${OUTFILE_ARM}" -ldflags '-w -s -extldflags "-static"' .
+echo "Building for arm32..."
+GOOS=linux GOARCH=arm GOARM=7 go build -o "dist/${OUTFILE_ARM_32}" -ldflags '-w -s -extldflags "-static"' .
+
+echo "Building for arm64..."
+GOOS=linux GOARCH=arm64 go build -o "dist/${OUTFILE_ARM_64}" -ldflags '-w -s -extldflags "-static"' .
 
 echo "Building for windows..."
 GOOS=windows GOARCH=amd64 go build -o "dist/${OUTFILE_WINDOWS}" -ldflags '-w -s -extldflags "-static"' .
